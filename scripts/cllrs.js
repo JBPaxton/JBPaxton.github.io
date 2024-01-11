87a281d6b2a1c1713721176707709b731c3d686e
JBPaxton <48292685+JBPaxton@users.noreply.github.com>
2024-01-11 16:22:35 +0000

    Add files via upload

---
+++ scripts/cllrs.js
@@ -0,0 +1,9 @@
+let cllrs = {
+    "Ensor":"councillor@charlieensor.com",
+    "Hamilton":"kate.wupc@gmail.com",
+    "Mchaddan":"clairemchaddan.wupc@gmail.com",
+    "Quin":"jonathan.wupc@gmail.com",
+    "Roberts":"huwroberts.wupc@gmail.com",
+    "Thomas":"cllremmawupc@outlook.com",
+    "Clerk":"clerk.watersupton@parishcouncil.work"
+}
